4f8103baffec5e71b03a6a08466701cc36faaf90
Pooja Gurung <pooja_grg07@hotmail.com>
2024-02-15 16:28:16 +0000

    Initial commit

---
+++ components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Hero () {
+    return (
+        <section className="hero">
+            <img className="hero--photo" src="../images/photo-grid.png" />
+            <h4 className="hero--header"> Online Experiences </h4>
+            <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
+        </section>
+    )
+} 
