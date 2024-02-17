3d3309c1460106e340125e6a90fc808552cf8adc
Pooja Gurung <pooja_grg07@hotmail.com>
2024-02-17 10:22:05 +0000

    card component props used instead of actual props

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,18 +1,18 @@
 import React from "react"
 
-export default function Card({img, rating, reviews, location, title, price}) {
+export default function Card(props) {
     return (
         <div className="card">
             {/* src: inside jsx {} --> backticks `help us to use javascrpit & interpolate javascript object`  */}
-            <img className="card--photo" src={`../images/${img}`} alt="Main card photo"/>
+            <img className="card--photo" src={`../images/${props.img}`} alt="Main card photo"/>
             <div className="card--stats">
                 <img className="card--rating" src="../images/star.png" alt="Star icon"/>
-                <span className="card--gray">{rating}</span>
-                <span className="card--gray">{reviews} • </span>
-                <span>{location}</span>
+                <span className="card--gray">{props.rating}</span>
+                <span className="card--gray">{props.reviewCount} • </span>
+                <span>{props.location}</span>
             </div>
-            <h2>{title}</h2>
-            <p><span className="card--bold"> From ${price}</span> / person</p>
+            <h2>{props.title}</h2>
+            <p><span className="card--bold"> From ${props.price}</span> / person</p>
         </div>
     )
 }
