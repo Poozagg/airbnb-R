6af40835e96f95ec999f51421b647a8ef3614940
Pooja Gurung <pooja_grg07@hotmail.com>
2024-02-19 14:47:48 +0000

    same again

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function Hero () {
     return (
         <section className="hero">
-            <img className="hero--photo" src="../images/photo-grid.png" />
+            <img className="hero--photo" src="./images/photo-grid.png" />
             <h4 className="hero--header"> Online Experiences </h4>
             <p className="hero--text">Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
         </section>
